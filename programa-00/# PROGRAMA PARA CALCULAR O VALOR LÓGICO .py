05519a7091fbcef074486c1e047e4ef994a79526
fermartinsbd-beep <fermartinsbd@gmail.com>
2026-08-30 21:49:56 -0300

    Adicionei um print ("testeeeee") no código para testar a função do programa.

---
+++ programa-00/# PROGRAMA PARA CALCULAR O VALOR LÓGICO .py
@@ -0,0 +1,14 @@
+# PROGRAMA PARA CALCULAR O VALOR LÓGICO DE:
+# P OR (Q AND R)
+p = input("Digite o valor de P (True/False): ").strip().lower() == 'true'
+q = input("Digite o valor de Q (True/False): ").strip().lower() == 'true'
+r = input("Digite o valor de R (True/False): ").strip().lower() == 'true'
+
+if p or (q and r):
+    print ("verdadeiro")
+
+
+    print ("testeeeee")
+
+else:
+    print("falso")
